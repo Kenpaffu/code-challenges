a678be68f9bf13b61a4cd0ad67f478a77291ab6e
Tony Villa <tonyvilla77@gmail.com>
2020-12-13 01:55:12 -0800

    more challenges

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function divisors(integer) {
+const divisors = (integer) => {
   let n = [];
   for (let i = 2; i < integer; i++) {
     if (integer % i === 0) n.push(i);
@@ -6,6 +6,20 @@ function divisors(integer) {
   if (n.length < 1 || n === undefined) {
     return `${integer} is prime`;
   } else return n;
-}
+};
 
-console.log(divisors(12));
+console.log(divisors(32));
+
+// CODE CHALLENGE
+// Tony's Space
+function litres(time) {
+  li = Math.floor(time * 0.5);
+  return li;
+}
+console.log(litres(6.7));
+
+// Code Challenge Palindrome
+const isPalindrome = (x) => {
+  return x === Number(String(x).split("").reverse().join(""));
+};
+console.log(isPalindrome(121));
