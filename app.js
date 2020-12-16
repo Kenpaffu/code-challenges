aa3a7aba7881429800eb04433cb5f85a74be5357
Tony Villa <tonyvilla77@gmail.com>
2020-12-15 19:15:53 -0800

    Code challenge 4

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+"use strict";
+
+// Code Challenge 1 (7kyu)
 const divisors = (integer) => {
   let n = [];
   for (let i = 2; i < integer; i++) {
@@ -10,16 +13,26 @@ const divisors = (integer) => {
 
 console.log(divisors(32));
 
-// CODE CHALLENGE
+// CODE CHALLENGE 2 (8kyu)
 // Tony's Space
 function litres(time) {
-  li = Math.floor(time * 0.5);
+  const li = Math.floor(time * 0.5);
   return li;
 }
 console.log(litres(6.7));
 
-// Code Challenge Palindrome
+// Code Challenge Palindrome (8kyu)
 const isPalindrome = (x) => {
   return x === Number(String(x).split("").reverse().join(""));
 };
 console.log(isPalindrome(121));
+
+// Code Challenge 4 (8kyu)
+function sum(numbers) {
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
+  return total;
+}
+console.log(sum([]));
